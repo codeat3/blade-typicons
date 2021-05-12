8cd557f53867a966b0c249ae239dfb1df7cd76a1
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-12 21:55:19 +0530

    Added support for config file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,10 +2,13 @@
 
 This changelog follows [the Keep a Changelog standard](https://keepachangelog.com).
 
+## 1.3.1 (2021-05-12)
+Added support for config file
+
 ## 1.0.1 (2021-04-08)
 updating the prefix to `typ` to avoid confict
-## 1.0.0 (2021-03-30)
 
+## 1.0.0 (2021-03-30)
 support for blade-icons v1.0.0 added.
 
 ## 0.1.0 (2021-03-30)
--- src/BladeTypiconsServiceProvider.php
+++ src/BladeTypiconsServiceProvider.php
@@ -6,25 +6,37 @@ namespace Codeat3\BladeTypicons;
 
 use BladeUI\Icons\Factory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Container\Container;
 
 final class BladeTypiconsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->callAfterResolving(Factory::class, function (Factory $factory) {
-            $factory->add('typicons', [
-                'path' => __DIR__.'/../resources/svg',
-                'prefix' => 'typ',
-            ]);
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-typicons', []);
+
+            $factory->add('typicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
+
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/blade-typicons.php', 'blade-typicons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-typ'),
-            ], 'blade-typ');
+                __DIR__.'/../resources/svg' => public_path('vendor/blade-typicons'),
+            ], 'blade-typicons');
+
+            $this->publishes([
+                __DIR__.'/../config/blade-typicons.php' => $this->app->configPath('blade-typicons.php'),
+            ], 'blade-typicons-config');
         }
     }
 }
