c6fa2e7b05464e5036654615aecc4ef7400e9cea
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-08-27 13:01:38 +0530

    updated to phpcsfixer 3

--- src/BladeTypiconsServiceProvider.php
+++ src/BladeTypiconsServiceProvider.php
@@ -19,7 +19,6 @@ final class BladeTypiconsServiceProvider extends ServiceProvider
 
             $factory->add('typicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
-
     }
 
     private function registerConfig(): void
