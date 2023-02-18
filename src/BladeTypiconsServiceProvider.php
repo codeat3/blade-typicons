04636ebfc3f4d4191e09a19e1ef5bb6bea4bd7f0
Swapnil Sarwe <swapnilsarwe@gmail.com>
2023-02-18 20:04:52 +0530

    Laravel 10 support (#2)
    * Laravel 10 support
    
    * Fix styling
    
    ---------
    
    Co-authored-by: swapnilsarwe <swapnilsarwe@users.noreply.github.com>

--- src/BladeTypiconsServiceProvider.php
+++ src/BladeTypiconsServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Codeat3\BladeTypicons;
 
 use BladeUI\Icons\Factory;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
 
 final class BladeTypiconsServiceProvider extends ServiceProvider
 {
@@ -17,24 +17,24 @@ final class BladeTypiconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-typicons', []);
 
-            $factory->add('typicons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('typicons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-typicons.php', 'blade-typicons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-typicons.php', 'blade-typicons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-typicons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-typicons'),
             ], 'blade-typ'); // TDOO: update this alias to `blade-typicons` in next major release
 
             $this->publishes([
-                __DIR__.'/../config/blade-typicons.php' => $this->app->configPath('blade-typicons.php'),
+                __DIR__ . '/../config/blade-typicons.php' => $this->app->configPath('blade-typicons.php'),
             ], 'blade-typicons-config');
         }
     }
